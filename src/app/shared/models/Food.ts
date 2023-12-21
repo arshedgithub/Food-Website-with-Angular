c4a97735b818429e1e1053890eaaa7976559b180
arshedgithub <arshedahmedmail@gmail.com>
2023-12-21 09:16:39 +0530

    add Food Model and food details

---
+++ src/app/shared/models/Food.ts
@@ -0,0 +1,19 @@
+export class Food {
+
+    id!:number;
+    name!:string;
+    price!:number;
+    tags!:string[];
+    favourite:boolean = false;
+    stars:number = 0;
+    imageUrl!:string;
+    origins!:string[];
+    cookTime!:string;
+
+    // id:number;
+
+    // constructor(id:number){
+    //     this.id = id;
+    // }
+
+}
