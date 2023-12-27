ef1c595b2d7f24f5a8ad3605f11e28fb51cd8589
arshedgithub <arshedahmedmail@gmail.com>
2023-12-27 17:39:41 +0530

    add activatedRoute

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 import { FoodService } from '../services/food/food.service';
 import { Food } from '../shared/models/Food';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent {
   foods:Food[] = [];
 
-  constructor(private foodService:FoodService){}
+  constructor(private foodService:FoodService, private route:ActivatedRoute){}
 
   ngOnInit():void {
-    this.foods = this.foodService.getAll();
+    this.route.params.subscribe(params => {
+      if(params['searchTerm']) this.foods = this.foodService.getAll().filter(food => food.name.toLowerCase().includes(params['searchTerm'].toLowerCase()));
+      else this.foods = this.foodService.getAll();
+    }); 
   }
 }
