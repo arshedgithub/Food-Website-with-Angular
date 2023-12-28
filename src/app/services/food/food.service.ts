3640ffa721f1a1dbdfe3e4de94d7b54cb37378bc
arshedgithub <arshedahmedmail@gmail.com>
2023-12-29 02:28:08 +0530

    complete foodPage

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -88,6 +88,10 @@ export class FoodService {
     return this.getAll().filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()));
   }
 
+  getFoodById(id:number): Food{
+    return this.getAll().find(food => food.id == id)!;
+  }
+
   getAllTags(): Tag[]{
     return [
       { name: "All", count: 14},
