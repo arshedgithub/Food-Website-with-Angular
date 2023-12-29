2101277b36fe2524f773f9580a13d5f8c7e2b077
arshedgithub <arshedahmedmail@gmail.com>
2023-12-29 15:09:27 +0530

    remove unnecessary codes

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { CartService } from '../services/cart/cart.service';
 import { Cart } from '../shared/models/Cart';
 import { CartItem } from '../shared/models/CartItem';
-import { FoodService } from '../services/food/food.service';
 
 @Component({
   selector: 'app-cart-page',
@@ -13,13 +12,7 @@ export class CartPageComponent implements OnInit {
 
   cart!:Cart;
 
-  constructor(private cartService:CartService, private foodService:FoodService){
-    
-    // temporary code to style cart page
-    let foods = foodService.getAll();
-    cartService.addToCart(foods[1]);
-    cartService.addToCart(foods[3]);
-    cartService.addToCart(foods[4]);
+  constructor(private cartService:CartService){
     
     this.setCart();
   }
