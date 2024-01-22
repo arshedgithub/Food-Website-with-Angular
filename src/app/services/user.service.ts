a60aa88385779bdd6c6eddeb77261f731a619f2f
arshedgithub <arshedahmedmail@gmail.com>
2024-01-23 00:48:11 +0530

    implementing post method for login

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { User } from '../shared/models/User';
+import { IUserLogin } from '../shared/Interfaces/IUserLogin';
+import { HttpClient } from '@angular/common/http';
+import { USER_LOGIN_URL } from '../shared/constants/urls';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  // read and write behaviour
+  private userSubject = new BehaviorSubject<User>(new User());
+  public userObservable:Observable<User>;
+  constructor(private http:HttpClient) { 
+    // can only read the user object
+    this.userObservable = this.userSubject.asObservable();
+  }
 
-  constructor() { }
+  // From the I in the begining: interface
+  // so IUserLogin is an interface
+  login(userLogin: IUserLogin):Observable<User>{
+    return this.http.post<User>(USER_LOGIN_URL, userLogin).pipe(
+      tap({
+        next: (user) => {},
+        error: (errorResponse) => {}
+      })
+    );
+  }
 }
---
+++ src/app/shared/Interfaces/IUserLogin.ts
@@ -0,0 +1,5 @@
+export interface IUserLogin {
+    // By defualt all the attributes in interfaces are required.
+    name: String;
+    email: String;
+}
